641781392abd50d5cf86e7796f1b95e3b8f35488
rohit001007 <rohit001007@gmail.com>
2020-11-24 13:12:54 +0530

    Added : The Part And Full Time Wage Of Employee

--- Employee.sh
+++ Employee.sh
@@ -2,13 +2,19 @@
 
 WagePerHr=20
 FullDayHrs=8
+PartTimeHrs=4
 
-var=$(($RANDOM%2))
+var=$(($RANDOM%3))
 
 if [ $var -eq 1 ]
 then
         dailywages=$(($WagePerHr*$FullDayHrs))
-	echo -ne "\n The Daily Wage Of Employee Is  : $dailywages\n"
+	echo -ne "\n The Full Day Wage Of Employee Is  : $dailywages\n"
+
+elif [ $var -eq 2 ]
+then
+        dailywages=$(($WagePerHr*$PartTimeHrs))
+        echo -ne "\n The Part Time Wage Of Employee Is  : $dailywages\n"
 
 else
         echo -ne "\n Employee Is Not Present...!\n"
